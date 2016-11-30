c653a46cc9fdf226ebf08dff2f314986e3642d6b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-11-30 01:09:35 +0000

    Add tests

---
+++ tests/phpunit/Service/ServiceTest.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Support\Collection;
+use Mockery as m;
+use PragmaRX\Health\ServiceProvider;
+
+class ServiceTest extends PHPUnit_Framework_TestCase
+{
+    private $service;
+
+    public function setUp()
+    {
+        $app = require __DIR__.'/../../../vendor/laravel/laravel/bootstrap/app.php';
+
+        $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+        $kernel->handle(
+            $request = Illuminate\Http\Request::capture()
+        );
+
+        $this->serviceProvider = $app->register(ServiceProvider::class);
+
+        $this->service = $this->serviceProvider->getHealthService();
+
+        $this->resources = $this->service->checkResources();
+    }
+
+    public function testInstantiation()
+    {
+        $this->assertInstanceOf(Collection::class, $this->resources);
+    }
+
+    public function testConfigWasLoadedProperly()
+    {
+        $this->assertEquals($this->resources['health']['error_message'], 'This is a test only error message.');
+    }
+
+    public function testResourcesHasTheCorrectCount()
+    {
+        $this->assertCount(9, $this->resources->toArray());
+    }
+
+    public function testResourcesItemsMatchConfig()
+    {
+        $this->assertEquals(
+            [
+                'health',
+                'cache',
+                'cloud_storage',
+                'database',
+                'filesystem',
+                'framework',
+                'http',
+                'https',
+                'mail',
+            ],
+            $this->resources->keys()->toArray()
+        );
+    }
+
+    public function testResourcesWhereChecked()
+    {
+        $healthCount = $this->resources->reduce(function ($carry, $item) {
+            return $carry + (isset($item['health']['healthy'])
+                    ? 1
+                    : 0);
+        }, 0);
+
+        $this->assertEquals(9, $healthCount);
+    }
+}
