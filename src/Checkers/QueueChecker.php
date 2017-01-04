c3fb9b159118fbca5b8491b736318b6f8542958a
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-01-04 20:28:07 -0200

    Add queue checker

---
+++ src/Checkers/QueueChecker.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace PragmaRX\Health\Checkers;
+
+use Queue;
+use Illuminate\Queue\Worker;
+use Illuminate\Queue\WorkerOptions;
+use PragmaRX\Health\Support\Jobs\TestJob;
+
+class QueueChecker extends BaseChecker
+{
+    /**
+     * Check resource.
+     *
+     * @return bool
+     */
+    public function check()
+    {
+        Queue::pushOn($this->resource['name'], app($this->resource['test_job']));
+
+        $worker = app(Worker::class);
+
+        $connection = $this->resource['connection'] ?: app('config')['queue.default'];
+
+        $queue = app('config')->get(
+            "queue.connections.{$connection}.queue", 'default'
+        );
+
+        $worker->setCache(app($this->resource['cache_instance'])->driver());
+
+        $worker->runNextJob(
+            $connection, $queue, $this->gatherWorkerOptions()
+        );
+
+        return $this->makeResult(true);
+    }
+
+    /**
+     * Gather all of the queue worker options as a single object.
+     *
+     * @return \Illuminate\Queue\WorkerOptions
+     */
+    protected function gatherWorkerOptions()
+    {
+        return new WorkerOptions(
+            false, false,
+            false, false,
+            false, false
+        );
+    }
+}
