d6a2c81812d4bfb2c93dc5fd72c9594cbafcee32
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-01-05 00:08:11 -0200

    Add Redis checker

---
+++ src/Checkers/RedisChecker.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PragmaRX\Health\Checkers;
+
+use Redis;
+
+class RedisChecker extends BaseChecker
+{
+    /**
+     * Check resource.
+     *
+     * @return bool
+     */
+    public function check()
+    {
+        Redis::set($key = $this->resource['key'], $number = random_bytes(80));
+
+        $result = Redis::get($key);
+
+        return $this->makeResult(
+            $number == $result,
+            $number !== $result ? $this->resource['key'] : ''
+        );
+    }
+}
