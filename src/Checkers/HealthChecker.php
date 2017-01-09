36ba5f03909838aa5541273a072a5dbd945c24c0
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-01-08 23:23:22 -0200

    Refactor

--- src/Checkers/HealthChecker.php
+++ src/Checkers/HealthChecker.php
@@ -5,19 +5,45 @@ namespace PragmaRX\Health\Checkers;
 class HealthChecker extends BaseChecker
 {
     /**
+     * Check health.
+     *
      * @return bool
      */
     public function check()
     {
-        $healthy = $this->resources->reduce(function ($carry, $item) {
-            return isset($item['is_global']) && $item['is_global']
-                    ? $carry
-                    : $carry && $item['health']['healthy'];
-        }, true);
+        $healthy = $this->isHealthy();
 
         return $this->makeResult(
             $healthy,
             $healthy ? '' : $this->resource['error_message']
         );
+    }
+
+    /**
+     * Compute health.
+     *
+     * @param $previous
+     * @param $current
+     * @return bool
+     */
+    private function computeHealth($previous, $current)
+    {
+        return isset($current['is_global']) && $current['is_global']
+            ? $previous
+            : $previous && $current['health']['healthy'];
+    }
+
+    /**
+     * Check if the resource is healty.
+     *
+     * @return mixed
+     */
+    protected function isHealthy()
+    {
+        $healthy = $this->resources->reduce(function ($carry, $item) {
+            return $this->computeHealth($carry, $item);
+        }, true);
+
+        return $healthy;
     }
 }
