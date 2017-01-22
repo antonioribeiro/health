eade1f54f3ab467212e66203750e092c5e247416
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-01-22 15:10:25 +0000

    Create a base html view

---
+++ src/views/html.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+
+        <title>{{ config('health.title') }}</title>
+
+        @yield('head')
+    </head>
+
+    <body>
+        @yield('body')
+    </body>
+</html>
