3c9731910d4348917ba5701cbeff84c1e7106e77
Ilya Gusev <mail@igusev.ru>
2019-10-04 05:58:54 +0300

    fixed problem wiht [] in list unpacking can be used in PHP 7.1 only

--- src/Checkers/Http.php
+++ src/Checkers/Http.php
@@ -40,7 +40,7 @@ class Http extends Base
             $health = [];
 
             foreach ($this->getResourceUrlArray() as $url) {
-                [$healthy, $message] = $this->checkWebPage(
+                list($healthy, $message) = $this->checkWebPage(
                     $this->makeUrlWithScheme($url, $this->secure),
                     $this->secure
                 );
