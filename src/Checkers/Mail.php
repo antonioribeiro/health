6f39d8c8d63e04689f064fa22490509b6ec6127f
nada <nada.h.farouk@gmail.com>
2019-11-03 12:03:41 +0200

    remove call to a laravel array_ helper -remove call to array_get to make it compatible with laravel 6

--- src/Checkers/Mail.php
+++ src/Checkers/Mail.php
@@ -4,6 +4,7 @@ namespace PragmaRX\Health\Checkers;
 
 use PragmaRX\Health\Support\Result;
 use Illuminate\Support\Facades\Mail as IlluminateMail;
+use Illuminate\Support\Arr;
 
 class Mail extends Base
 {
@@ -70,7 +71,7 @@ class Mail extends Base
     private function sendMail()
     {
         IlluminateMail::send($this->target->view, [], function ($message) {
-            $fromAddress = array_get($this->target->config, 'from.address');
+            $fromAddress = Arr::get($this->target->config, 'from.address');
 
             $message->returnPath($fromAddress);
 
